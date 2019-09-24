8ac881c5b7b5a323c5d0a687a6c65d067072a08c
Durianyang <49614355+Durianyang@users.noreply.github.com>
2019-09-24 10:30:21 +0800

    测试方法

---
+++ springDataJpa/src/test/java/test/JpqlTest.java
@@ -0,0 +1,107 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import xyz.durianyang.entity.Customer;
+import xyz.durianyang.service.CustomerService;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Durian
+ * @date 2019-09-23 18:47
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class JpqlTest
+{
+    @Autowired
+    private CustomerService customerService;
+
+    @Test
+    public void testFindByName()
+    {
+        Customer customer = customerService.findByName("李四");
+        System.out.println(customer);
+    }
+
+    @Test
+    public void testFindByNameAndById()
+    {
+        Customer customer = customerService.findByNameAndById("李四", 5L);
+        System.out.println(customer);
+    }
+
+    /**
+     * SpringDataJpa使用JPQL更新/删除需要事务支持
+     *      手动添加事务
+     *      并且默认回滚事务
+     */
+    @Test
+    public void testUpdateNameById()
+    {
+        customerService.updateNameById(5L, "王二");
+    }
+
+    /**
+     * 每个Object[]对象存储一条记录的数据
+     */
+    @Test
+    public void testFindBySql()
+    {
+        List<Object[]> customerArray = customerService.findBySql();
+        for (Object[] objects : customerArray)
+        {
+            System.out.println(Arrays.toString(objects));
+        }
+    }
+
+    @Test
+    public void findBySqlName()
+    {
+        List<Object[]> customerArray = customerService.findBySqlName("%王%");
+        for (Object[] objects : customerArray)
+        {
+            System.out.println(Arrays.toString(objects));
+        }
+    }
+
+    /**
+     * 使用方法命名规则查询,findBy 开头，表示查询,默认使用=查询而不是用LIKE
+     *  + 对象中的属性名，首字母大写
+     *  + 查询方式(LIKE/isNull)
+     *  + 多条件连接符
+     *  含义：根据对应属性查询
+     */
+    @Test
+    public void findByCustName()
+    {
+        Customer customer = customerService.findByCustName("王五");
+        System.out.println(customer);
+    }
+
+    @Test
+    public void findByCustNameLike()
+    {
+        List<Customer> customerList = customerService.findByCustNameLike("%五%");
+        for (Customer customer : customerList)
+        {
+            System.out.println(customer);
+        }
+    }
+
+    @Test
+    public void findByCustNameLikeAndCustAddress()
+    {
+        List<Customer> customerList = customerService.findByCustNameLikeAndCustAddress("%王%", "长沙   ");
+        for (Customer customer : customerList)
+        {
+            System.out.println(customer);
+        }
+    }
+
+}
